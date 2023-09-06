d5c91d4a4184956a32017c45a14d7dd0af087f52
Chandanac414 <chandanac414@gmail.com>
2023-09-07 01:59:41 +0530

    only shows the temperature of city

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
+import json
 city=input("enter the city name")
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=0b81d6002f95fc5f58e08fccf865166e"
 r = requests.get(url)
-print(r.text)
+# print(r.text)
+dict1 = json.loads(r.text)
+# print(dict1)
+dict2 = dict1["main"]
+print("temperature of",city,"is ",dict2["temp"])
