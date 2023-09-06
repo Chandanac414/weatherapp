1224f8755d98558c4cc06830bf7940d72faa16e7
Chandanac414 <chandanac414@gmail.com>
2023-09-07 02:12:13 +0530

    can tell the temperature of the city

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 import json
-city=input("enter the city name")
+import win32com.client
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+city=input("Enter the city name:\n")
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=0b81d6002f95fc5f58e08fccf865166e"
 r = requests.get(url)
 # print(r.text)
 dict1 = json.loads(r.text)
 # print(dict1)
 dict2 = dict1["main"]
-print("temperature of",city,"is ",dict2["temp"])
+x=f'Temperature of {city} is {dict2["temp"]}'
+print(x)
+speaker.Speak(x)
